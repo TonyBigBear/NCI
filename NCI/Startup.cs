6a256d820235a710787502cec905f05c1433b270
SmithTJ <SmithTJ@DUBW00000222>
2017-05-03 12:31:59 +0100

    Add project files.

---
+++ NCI/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(NCI.Startup))]
+namespace NCI
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
